2325bfce9592aa65d72d910b958bf60b46e0fbf1
Denars <51354651+Denars@users.noreply.github.com>
2019-07-08 22:45:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('Hello Mediasoft');
+//alert('Hello Mediasoft');
+
+let fN = prompt("Введите первое число", "");
+let sN = prompt("Введите второе число", "");
+let operator = prompt("Введите знак + - * /", "");
+
+if (operator == '+') {
+	result = +fN + +sN;
+} else if(operator == '-') {
+	result = fN - sN;
+} else if(operator == '*') {
+	result = fN * sN;
+} else if(operator == '/') {
+	result = fN / sN;
+} else {
+	alert("не верная операция");
+}
+
+alert('Итого ' + result);
